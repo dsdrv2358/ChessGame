bd8af169df9555d78d00b6a7279b7de051448d8b
Monty <yankeesfan4life18@gmail.com>
2025-04-18 10:51:46 -0400

    queen now moves like a bishop combined with a rook

--- Assets/Scripts/ChessPieces/Queen.cs
+++ Assets/Scripts/ChessPieces/Queen.cs
@@ -1,5 +1,136 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Queen : ChessPiece
 {
+    public override List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] board, int tileCountX, int tileCountY)
+    {
+        List<Vector2Int> r = new List<Vector2Int>();
+    
+        // Down
+        for (int i = currentY - 1; i >= 0; i--)
+        {
+            // If squares are empty
+            if (board[currentX, i] == null)
+                r.Add(new Vector2Int(currentX, i));
+
+            // If they have a piece on them
+            if (board[currentX, i] != null)
+            {
+                if (board[currentX, i].team != team)
+                    r.Add(new Vector2Int(currentX, i));
+
+                break;
+            }
+        }
+
+        // Up
+        for (int i = currentY + 1; i < tileCountY; i++)
+        {
+            // If squares are empty
+            if (board[currentX, i] == null)
+                r.Add(new Vector2Int(currentX, i));
+
+            // If they have a piece on them
+            if (board[currentX, i] != null)
+            {
+                if (board[currentX, i].team != team)
+                    r.Add(new Vector2Int(currentX, i));
+
+                break;
+            }
+        }
+
+        // Left
+        for (int i = currentX - 1; i >= 0; i--)
+        {
+            // If squares are empty
+            if (board[i, currentY] == null)
+                r.Add(new Vector2Int(i, currentY));
+
+            // If they have a piece on them
+            if (board[i, currentY] != null)
+            {
+                if (board[i, currentY].team != team)
+                    r.Add(new Vector2Int(i, currentY));
+
+                break;
+            }
+        }
+
+        // Right
+        for (int i = currentX + 1; i < tileCountX; i++)
+        {
+            // If squares are empty
+            if (board[i, currentY] == null)
+                r.Add(new Vector2Int(i, currentY));
+
+            // If they have a piece on them
+            if (board[i, currentY] != null)
+            {
+                if (board[i, currentY].team != team)
+                    r.Add(new Vector2Int(i, currentY));
+
+                break;
+            }
+        }
+        
+        // Top Right
+        for (int x = currentX + 1, y = currentY + 1; x < tileCountX && y < tileCountY; x++, y++)
+        {
+            if (board[x,y] == null)
+                r.Add(new Vector2Int(x,y));
+            else
+            {
+                if (board[x,y].team != team)
+                    r.Add(new Vector2Int(x,y));
+
+                break;
+            }    
+        }
+
+        // Top Left
+        for (int x = currentX - 1, y = currentY + 1; x >= 0 && y < tileCountY; x--, y++)
+        {
+            if (board[x,y] == null)
+                r.Add(new Vector2Int(x,y));
+            else
+            {
+                if (board[x,y].team != team)
+                    r.Add(new Vector2Int(x,y));
+
+                break;
+            }    
+        }
+
+        // Bottom Right
+        for (int x = currentX + 1, y = currentY - 1; x < tileCountX && y >= 0; x++, y--)
+        {
+            if (board[x,y] == null)
+                r.Add(new Vector2Int(x,y));
+            else
+            {
+                if (board[x,y].team != team)
+                    r.Add(new Vector2Int(x,y));
+
+                break;
+            }    
+        }
+
+        // Bottom Left
+        for (int x = currentX - 1, y = currentY - 1; x >= 0 && y >= 0; x--, y--)
+        {
+            if (board[x,y] == null)
+                r.Add(new Vector2Int(x,y));
+            else
+            {
+                if (board[x,y].team != team)
+                    r.Add(new Vector2Int(x,y));
+
+                break;
+            }    
+        }
+
+        return r;
+    }
 }
