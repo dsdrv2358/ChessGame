88e77f4293e8aa4d5d3ee372be9710b5431fd234
Monty <yankeesfan4life18@gmail.com>
2025-04-18 08:33:28 -0400

    Pawns can now only move one square forward

--- Assets/Scripts/ChessPieces/Pawn.cs
+++ Assets/Scripts/ChessPieces/Pawn.cs
@@ -1,5 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Pawn : ChessPiece
 {
+    public override List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] board, int tileCountX, int tileCountY)
+    {
+        List<Vector2Int> r = new List<Vector2Int>();
+
+        int direction = (team == 0) ? 1 : -1;
+
+        // One in front
+        if (board[currentX,currentY + direction] == null)
+            r.Add(new Vector2Int(currentX, currentY + direction));
+
+        return r;
+    }
 }
