50726faacd07c3e5c27245918392ae485f723f05
Monty <yankeesfan4life18@gmail.com>
2025-04-20 19:21:34 -0400

    netMessage classs implemented

--- Assets/Scripts/Net/NetMessages/NetMessage.cs
+++ Assets/Scripts/Net/NetMessages/NetMessage.cs
@@ -1,6 +1,34 @@
-using UnityEngine;
+using Unity.Collections;
+using Unity.Networking.Transport;
+
+public enum OpCode
+{
+    KEEP_ALIVE = 1,
+    WELCOME = 2,
+    START_GAME = 3,
+    MAKE_MOVE = 4,
+    REMATCH = 5
+}
 
 public class NetMessage
 {
-    
+    public OpCode Code { set; get; }
+
+    public virtual void Serialize(ref DataStreamWriter writer)
+    {
+        writer.WriteByte((byte)Code);
+    }
+    public virtual void Deserialize(ref DataStreamReader reader)
+    {
+
+    }
+
+    public virtual void ReceivedOnClient()
+    {
+
+    }
+    public virtual void ReceivedOnServer(NetworkConnection cnn)
+    {
+
+    }
 }
