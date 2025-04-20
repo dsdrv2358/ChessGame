c7f3961522211b35342e065824f84ab976ae61c5
Monty <yankeesfan4life18@gmail.com>
2025-04-20 19:25:43 -0400

    move opCOde enum from netMessage to NetUtility

--- Assets/Scripts/Net/NetMessages/NetMessage.cs
+++ Assets/Scripts/Net/NetMessages/NetMessage.cs
@@ -1,15 +1,6 @@
 using Unity.Collections;
 using Unity.Networking.Transport;
 
-public enum OpCode
-{
-    KEEP_ALIVE = 1,
-    WELCOME = 2,
-    START_GAME = 3,
-    MAKE_MOVE = 4,
-    REMATCH = 5
-}
-
 public class NetMessage
 {
     public OpCode Code { set; get; }
--- Assets/Scripts/Net/NetUtility.cs
+++ Assets/Scripts/Net/NetUtility.cs
@@ -1,4 +1,11 @@
-using UnityEngine;
+public enum OpCode
+{
+    KEEP_ALIVE = 1,
+    WELCOME = 2,
+    START_GAME = 3,
+    MAKE_MOVE = 4,
+    REMATCH = 5
+}
 
 public static class NetUtility
 {
