5d8e2d30fa56190b3007da9be9f61dd4c5655ab1
Monty <yankeesfan4life18@gmail.com>
2025-04-20 18:37:55 -0400

    client now has singleton implementation

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class Client : MonoBehaviour
 {
-    
+    #region Singleton implementation
+    public static Client Instance { set; get; }
+    private void Awake()
+    {
+        Instance = this;
+    }
+    #endregion
 }
