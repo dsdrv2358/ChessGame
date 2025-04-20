87c9fe6791767a3440f46c56cc9d0508c0f4d0d8
Monty <yankeesfan4life18@gmail.com>
2025-04-20 17:15:49 -0400

    we want the server to be a diy singleton

--- Assets/Scripts/Server.cs
+++ Assets/Scripts/Server.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
 
-public class Server
+public class Server : MonoBehaviour
 {
-    
+    #region Singleton implementation
+    public static Server Instance { set; get; }
+    private void Awake()
+    {
+        Instance = this;
+    }
+    #endregion
 }
